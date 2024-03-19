30f29fc6454e70d729d5ad28fb0c95d4582fbd54
Nilesh Mittha <100112399+nileshmittha@users.noreply.github.com>
2024-03-19 22:52:02 +0530

    Add files via upload

---
+++ SortInDecending.php
@@ -0,0 +1,9 @@
+<?php
+$fruits = array("apple", "orange", "banana", "mango");
+
+rsort($fruits);
+
+foreach ($fruits as $fruit) {
+    echo $fruit . "\n";
+}
+?>
---
+++ arraySort.php
@@ -0,0 +1,11 @@
+<?php
+
+$fruits = array("apple", "orange", "banana", "mango");
+
+
+sort($fruits);
+
+foreach ($fruits as $fruit) {
+    echo $fruit . "\n";
+}
+?>
